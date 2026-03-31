88e874242b04c8bc8b62ca4b33b9f2ed542bf282
pathmaker04 <workform04@gmail.com>
2026-03-31 17:54:18 +0700

    update

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -14,7 +14,7 @@ const __dirname = path.dirname(__filename);
 const app = express();
 
 app.use(cors({
-  origin: 'expense-tracker-mdbj6kl9o-pathmaker04s-projects.vercel.app',
+  origin: 'https://expense-tracker-git-main-pathmaker04s-projects.vercel.app',
   credentials: true
 }));
 
